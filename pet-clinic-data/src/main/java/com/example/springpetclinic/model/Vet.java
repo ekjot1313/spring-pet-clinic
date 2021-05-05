4536682e2ea9b138d3aa8593eceb3c594c262528
Ekjot <ekjotsingh00776@gmail.com>
2021-05-05 15:08:18 -0400

    Updated Vet with JPA annotations. Closes #36

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Vet.java
@@ -1,11 +1,18 @@
 package com.example.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private Set<Specialty> specialties=new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
         return specialties;
