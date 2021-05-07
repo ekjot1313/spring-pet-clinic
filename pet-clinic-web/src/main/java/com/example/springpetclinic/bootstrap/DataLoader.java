ecafab512526b9e6c6a8cbccde016d25e20debca
Ekjot <ekjotsingh00776@gmail.com>
2021-05-07 16:12:34 -0400

    Implemented Spring Data JPA Impl for Visit Service. Closes #46

--- pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.example.springpetclinic.bootstrap;
 
 import com.example.springpetclinic.model.*;
-import com.example.springpetclinic.services.OwnerService;
-import com.example.springpetclinic.services.PetTypeService;
-import com.example.springpetclinic.services.SpecialtyService;
-import com.example.springpetclinic.services.VetService;
+import com.example.springpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Visit catVisit=new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
+
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
@@ -105,5 +111,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Loaded Vets...");
+
     }
 }
